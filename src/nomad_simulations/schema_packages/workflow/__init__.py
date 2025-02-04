3e46cccf9bf64fa8e6fa7a7dc816775906239142
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2025-02-04 15:51:56 +0100

    Add md

--- src/nomad_simulations/schema_packages/workflow/__init__.py
+++ src/nomad_simulations/schema_packages/workflow/__init__.py
@@ -1,4 +1,6 @@
 from .general import SimulationWorkflow
 from .geometry_optimization import GeometryOptimization
 from .gw import DFTGWWorkflow
+from .molecular_dynamics import MolecularDynamics
 from .single_point import SinglePoint
+from .thermodynamics import Thermodynamics
